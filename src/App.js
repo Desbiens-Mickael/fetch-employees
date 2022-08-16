1eefe0ebd1b7d3cef019b1a2a55d98ece8a8fd30
Mickael Desbiens <desbiens.mickael01@gmail.com>
2022-08-16 11:39:15 +0200

    use axios to consume an api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import DisplayEmployee from './components/DisplayEmployee';
+
+const sampleEmployee = {
+  gender: 'male',
+  name: {
+    first: 'Charlie',
+    last: 'Thompson',
+    title: 'M'
+  },
+  location: {
+    street: {
+      number: 761,
+      name: 'Tay Street',
+    },
+    city: 'Timaru',
+    postcode: 76111,
+  },
+  email: 'charlie.thompson@example.com',
+  picture: {
+    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
+  },
+};
 
 function App() {
+  const [employee, setEmployee] = useState(sampleEmployee);
+
+  const getEmployee = () => {
+    // Envoyer la demande
+    axios
+      .get('https://randomuser.me/api?nat=en')
+      // Extraire les DONNÉES de la réponse reçue
+      .then((response) => response.data)
+      // Utilisez ces données pour mettre à jour l'état
+      .then((data) => {
+        setEmployee(data.results[0]);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DisplayEmployee employee={employee} />
+      <button type="button" onClick={getEmployee}>Get employee</button>
     </div>
   );
 }
